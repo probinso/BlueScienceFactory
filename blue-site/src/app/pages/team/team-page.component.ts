5b57372fb8334aa9aa171ec4a3ae612506acd3c6
probinso <pmoss.robinson@gmail.com>
2019-05-18 00:20:17 -0700

    update

--- blue-site/src/app/pages/team/team-page.component.ts
+++ blue-site/src/app/pages/team/team-page.component.ts
@@ -4,4 +4,12 @@ import { Component } from "@angular/core";
   selector: "team-page",
   templateUrl: "./team-page.component.html"
 })
-export class TeamPageComponent {}
+export class TeamPageComponent {
+  team = [
+    {
+      name: "Philip",
+      img:
+        "https://media.licdn.com/dms/image/C5603AQFCCEr1_Ca6IQ/profile-displayphoto-shrink_200_200/0?e=1563408000&v=beta&t=itYaU8C25L6tVU2I-nOw9PMqWWQKFkmWHztejVGtpJ4"
+    }
+  ];
+}
