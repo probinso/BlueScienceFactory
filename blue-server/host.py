c5abb6d83b2cdc3eea118c7a8a319fe875af91be
probinso <pmoss.robinson@gmail.com>
2019-11-30 21:00:50 -0800

    better ops

--- blue-server/host.py
+++ blue-server/host.py
@@ -8,6 +8,7 @@ from flask_cors import CORS
 # from flask_restful import Resource, Api
 
 from aco.aco import ACOio, datetime
+# from aco.aco import timedelta
 
 
 app = Flask(__name__)
@@ -18,13 +19,17 @@ BASEDIR = '/home/probinso/git/kellen-whales'
 aco = ACOio(BASEDIR)
 
 
-@app.route('/retrieve/')
-@app.route('/retrieve/<string:timestr>/')
-def retrieve(timestr='02:23:2016:22:07:26'):
+@app.route('/retrieve/raw/')
+@app.route('/retrieve/raw/<string:timestr>/')
+@app.route('/retrieve/raw/<string:timestr>/<int:seconds>')
+def retrieve(
+    timestr='02:23:2016:22:07:26',
+    # seconds=None
+):
     timestamp = datetime.strptime(timestr, "%m:%d:%Y:%H:%M:%S")
     try:
         result = aco.load(timestamp)
-        filtered = result.highpass().lowpass()
+        filtered = result.resample_fs(1600)
 
         def stream(io, step=4096):
             print("stream start", file=stderr)
@@ -33,10 +38,11 @@ def retrieve(timestr='02:23:2016:22:07:26'):
                 yield packet
                 packet = io.read(step)
             print("stream end", file=stderr)
-        return Response(stream(filtered.get_wav()), mimetype=result.header)
+
+        return Response(stream(filtered.get_wav()), mimetype=filtered.header)
     except FileNotFoundError as ex:
         print(ex, file=stderr)
-        return Response(None, mimetype=result.header)
+        return Response(None)
 
 
 @app.after_request
