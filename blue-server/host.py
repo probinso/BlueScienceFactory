593f55927ca751e09494d6b7a27c5b9ba21c533c
probinso <pmoss.robinson@gmail.com>
2019-12-15 20:43:34 -0800

    updates

--- blue-server/host.py
+++ blue-server/host.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 # from flask_restful import Resource, Api
 
 from aco.aco import ACOio, datetime
-# from aco.aco import timedelta
+from aco.aco import timedelta
 
 
 app = Flask(__name__)
@@ -19,27 +19,32 @@ BASEDIR = '/home/probinso/git/kellen-whales'
 aco = ACOio(BASEDIR)
 
 
+def stream(io, step=4096):
+    # print("stream start", file=stderr)
+    packet = io.read(step)
+    while packet:
+        yield packet
+        packet = io.read(step)
+    # print("stream end", file=stderr)
+
+
 @app.route('/retrieve/raw/')
 @app.route('/retrieve/raw/<string:timestr>/')
 @app.route('/retrieve/raw/<string:timestr>/<int:seconds>')
 def retrieve(
     timestr='02:23:2016:22:07:26',
-    # seconds=None
+    seconds=timedelta(minutes=6).total_seconds()
 ):
     timestamp = datetime.strptime(timestr, "%m:%d:%Y:%H:%M:%S")
     try:
-        result = aco.load(timestamp)
-        filtered = result
-
-        def stream(io, step=4096):
-            print("stream start", file=stderr)
-            packet = io.read(step)
-            while packet:
-                yield packet
-                packet = io.read(step)
-            print("stream end", file=stderr)
-
-        return Response(stream(filtered.get_wav()), mimetype=filtered.header)
+        result = aco.load(timestamp, seconds)
+        op = result.get_wav
+        with open(f'boo.{op.extension}', 'wb') as fd:
+            fd.write(op().read())
+        return Response(
+            stream(op()),
+            mimetype=op.mime_type
+        )
     except FileNotFoundError as ex:
         print(ex, file=stderr)
         return Response(None)
